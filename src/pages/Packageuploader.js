3de97c1c06a17b7defa772d77defdd232e428af5
Kothakota Deepika <kothakota.deepika_ext@mediakind.com>
2024-03-22 16:59:45 +0530

    UI for Package uploader Tab

--- src/pages/Packageuploader.js
+++ src/pages/Packageuploader.js
@@ -76,7 +76,7 @@
 
 // export default Packageuploader
 
-import React, { useCallback } from 'react';
+import React, { useState, useEffect,useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import axios from 'axios'; // Import axios for making HTTP requests
 // import "./Packageuploader.css"
@@ -102,41 +102,100 @@ const Packageuploader = () => {
   }, []);
 
   const {getRootProps, getInputProps} = useDropzone({ onDrop, multiple: false, directory: true });
+  const [packageInfo, setPackageInfo] = useState(null);
 
-  return ( 
-  <div style={{ display: 'flex', alignItems: 'center',flexDirection:'column' }}>
-    {/* <caption>Packageuploader</caption> */}
-      <button {...getRootProps()} style={{
-      border: '2px solid #cccccc',
-      borderRadius: '8px',
-      padding: '20px',
-      width:'268px',
-      fontSize:'20px',
-      cursor: 'pointer',
-      margin:'140px',
-      marginLeft:'260px',
-      alignContent:'center',
-      justifyContent:'center',
-      textAlign: 'center',
-      position: 'relative',
-      background:'rgb(105, 88, 154)'
-    }}> {/* Apply CSS class */}
-        <input {...getInputProps()}/>
-        <p style={{
+  useEffect(() => {
+    const text = `Support package started: 2024-02-09T06:04:37.812579
+Added collector: DatabaseCollector
+Added collector: LogsCollector
+Added collector: PackagerCollector
+Added collector: EncodingLiveCollector
+Added collector: AlarmsCollector
+Added collector: StreamProcessorCollector
+Added collector: KubeCollector
+Added collector: HostCollector
+Added collector: RabbitMQCollector
+Support package ended: 2024-02-09T06:05:19.224497`;
+
+    const lines = text.split('\n');
+
+    let startDate, endDate;
+
+    for (let line of lines) {
+      if (line.includes('Support package started')) {
+        startDate = line.split(': ')[1];
+      } else if (line.includes('Support package ended')) {
+        endDate = line.split(': ')[1];
+      }
+    }
+
+    if (startDate && endDate) {
+      setPackageInfo({ startDate, endDate });
+    }
+  }, []);
+
+
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <div style={{marginLeft:'90px'}}>
+      {/* added now */}
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '-5px' }}>
+        <p style={{ marginRight: '100px',marginBottom:'-85px' }}><b>Upload Support Package</b></p>
+        <div><button {...getRootProps()} style={{
+          border: '2px solid #cccccc',
+          borderRadius: '8px',
+          padding: '20px',
+          width: '275px',
+          fontSize: '20px',
+          cursor: 'pointer',
+          margin: '100px',
+          marginLeft: '80px',
+          alignContent: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          position: 'relative',
+          background: 'rgb(105, 88, 154)',
+          marginBottom:'20px'
+        }}>
+          {/* Apply CSS class */}
+          <input {...getInputProps()} />
+          <p style={{
             position: 'relative',
             top: '50%',
             left: '50%',
-            right:'20%',
+            right: '20%',
             transform: 'translate(-50%, -50%)',
-            fontSize:'20px'
+            fontSize: '20px'
           }}>Upload Your Package</p>
-      </button>
-      <p style={{margin:'-120px',marginLeft:'10px'}}><b>Instructions: </b>"Zip" the Package before Uploading<br></br><i><u>example</u></i>:
-mksp1-support_package_2024-02-09T06_04_37.zip</p>
+        </button>
+        </div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+        <p style={{ marginRight: '130px'}}><b>Support Package Creation Time</b></p>
+        <div style={{ border: '1px solid black', padding: '5px', backgroundColor: 'rgb(105, 88, 154)', color: 'white', marginLeft: '-5px',width:'273px' }}>
+          {packageInfo ? packageInfo.endDate : "Loading package info..."}
+        </div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+        <p style={{ marginRight: '230px' }}><b>Reported Issue</b></p>
+        <div><input style={{ border: '1px solid black', padding: '5px', backgroundColor: 'rgb(105, 88, 154)', color: 'white',marginLeft:'30px',width:'273px' }}>
+        </input></div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+        <p style={{ marginRight: '115px' }}><b>Reported Issue Date and Time</b></p>
+        <div><input style={{ border: '1px solid black', padding: '5px', backgroundColor: 'rgb(105, 88, 154)', color: 'white',marginLeft:'24px' ,width:'273px'}}>
+        </input></div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+        <p style={{ marginRight: '40px' }}><b>Reported Issue Date and Time threshold</b></p>
+        <div><input style={{ border: '1px solid black', padding: '5px', backgroundColor: 'rgb(105, 88, 154)', color: 'white' ,marginLeft:'16px',width:'273px'}}>
+        </input></div>
+      </div>
+      </div>
     </div>
   );
-};
-
+        };  
 export default Packageuploader;
 
 
